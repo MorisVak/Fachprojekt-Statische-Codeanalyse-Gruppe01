4f02583dcb4e683b149b99fffdef87d5aae7abe8
maurice.mecke <maurice.mecke@yahoo.de>
2025-04-26 16:10:37 +0200

    finished god class analyzer

--- src/main/scala/Woche1/godclass.scala
+++ src/main/scala/Woche1/godclass.scala
@@ -3,6 +3,7 @@ import org.opalj.br.analyses.Project
 import org.opalj.da.ClassFileReader.{ClassFile, attributeType, fieldInfoType}
 import org.opalj.da.{Fields, Methods}
 
+import scala.collection.mutable
 import scala.io.StdIn
 
 object godclass {
@@ -10,27 +11,35 @@ object godclass {
   def main(args: Array[String]): Unit = {
     val jarFile = new java.io.File("pdfbox-2.0.24.jar")
 
-    println("-----------GOD CLASS ANALYZER-----------")
+    println("-----------GOD CLASS ANALYZER-----------\n")
 
     //getting the input of the user
-    println("please enter fields threshold")
+    println("please enter fields threshold : ")
     val fieldsThreshold = StdIn.readLine().toInt
     println(s"You entered ${fieldsThreshold}")
-    println("please enter a methods threshold")
+    println("please enter a methods threshold : ")
     val methodsThreshold = StdIn.readLine().toInt
     println(s"You entered ${methodsThreshold}")
+    println("please enter a fields of different objects thershold : ")
+    val differentClassFieldsThreshold = StdIn.readLine().toInt
+    println(s"You entered ${differentClassFieldsThreshold}")
+
+    //Result map that saves all potential god classes
+    val results = mutable.Map[String, Array[String]]()
+
+    //Count of all potential god classes
+    var godClassCount = 0
+
+
     val project = Project(jarFile)
     println(s"Total amount of classes in pdfbox-2.0.24.jar :  ${project.classFilesCount}")
     val classFiles = project.allClassFiles
     //Iterate over all the classes
     classFiles.foreach { classFile =>
+      results += (classFile.toString() -> Array.empty[String])
       //Count amount of fields
       val countFields = totalFields(classFile.fields)
-      /*classFile.fields.foreach{field =>
-          println(s"FIELD NAME : ${field.name} FIELD TYPE : ${field.fieldType} " +
-            s"IS BASE TYPE : ${field.fieldType.isBaseType} IS REFERENCE TYPE : ${field.fieldType.isReferenceType}")
-        }*/
-      val yes = checkAndCountForeignFieldTypes(classFile.fields, classFile)
+      val countDifferentObjectFields = checkAndCountForeignFieldTypes(classFile.fields, classFile)
       //count amount of methods
       val countMethods = totalClasses(classFile.methods)
       //val totalLoc = 0
@@ -40,14 +49,57 @@ object godclass {
         println("CLASS EXCEEDED FIELDS THRESHOLD = might be a god class")
         println(s"Fields Threshold = ${fieldsThreshold}")
         println(s"Count of FIELDS for Class ${classFile} = ${countFields} ")
+        // Get the current array (or an empty array if missing)
+        val currentArray = results.getOrElse(classFile.toString(), Array[String]())
+
+        // Create a new array by concatenating the new value
+        val updatedArray = currentArray :+ s"Field Count EXCEEDED : ${countFields} instead of ${fieldsThreshold}"
+
+        // Update the map entry
+        results.update(classFile.toString(), updatedArray)
       }
+
       if (countMethods > methodsThreshold) {
         println("CLASS EXCEEDED METHODS THRESHOLD = might be a god class")
         println(s"Methods Threshold = ${methodsThreshold}")
         println(s"Count of METHODS for Class ${classFile} = ${countMethods} ")
+        // Get the current array (or an empty array if missing)
+        val currentArray = results.getOrElse(classFile.toString(), Array[String]())
+
+        // Create a new array by concatenating the new value
+        val updatedArray = currentArray :+ s"Method Count EXCEEDED : ${countMethods} instead of ${methodsThreshold}"
+
+        // Update the map entry
+        results.update(classFile.toString(), updatedArray)
+      }
+      if(countDifferentObjectFields > differentClassFieldsThreshold){
+        println("CLASS EXCEEDEDS DIFFERENT OBJECT FIELDS = might be a god class")
+        println(s"Different Object Threshold = ${differentClassFieldsThreshold}")
+        println(s"Count of Different Objects for Class ${classFile} = ${countDifferentObjectFields} ")
+        // Get the current array (or an empty array if missing)
+        val currentArray = results.getOrElse(classFile.toString(), Array[String]())
+
+        // Create a new array by concatenating the new value
+        val updatedArray = currentArray :+ s"Different Object Count EXCEEDED : ${countDifferentObjectFields} instead of" +
+          s" ${differentClassFieldsThreshold}"
+
+        // Update the map entry
+        results.update(classFile.toString(), updatedArray)
       }
     }
-    println(s"Project Class File Count ${project.classFilesCount}")
+    println(s"\nProject Class File Count ${project.classFilesCount}")
+    println("-----------RESULT-----------")
+    results.foreach { case (key, array) =>
+      // Only print if array is not empty
+      if (array.nonEmpty) {
+        godClassCount += 1
+        println(s"\n$key:")
+        array.foreach(value => println(s"  - $value"))
+        println("_____________________________________________________")
+      }
+    }
+
+    println(s"\nTotal potential god classes : ${godClassCount} of ${project.classFilesCount} ")
   }
 
   def totalClasses(methods: br.Methods): Int = {
@@ -58,8 +110,8 @@ object godclass {
   def totalFields(fields: br.Fields): Int = {
     val count = fields.count(field => true)
     fields.foreach { field =>
-      println(s"FIELD NAME : ${field.name} FIELD TYPE : ${field.fieldType} " +
-        s"IS BASE TYPE : ${field.fieldType.isBaseType} IS REFERENCE TYPE : ${field.fieldType.isReferenceType}")
+      //println(s"FIELD NAME : ${field.name} FIELD TYPE : ${field.fieldType} " +
+        //s"IS BASE TYPE : ${field.fieldType.isBaseType} IS REFERENCE TYPE : ${field.fieldType.isReferenceType}")
     }
     return count
   }
@@ -67,25 +119,18 @@ object godclass {
   def checkAndCountForeignFieldTypes (fields: br.Fields, fieldClass : org.opalj.br.ClassFile): Int = {
     var count = 0
 
-    fieldClass.thisType
     fields.foreach{field =>
       if(field.fieldType.isReferenceType){
         if(!field.fieldType.toString.contains("java/") && !field.fieldType.isBaseType && !field.fieldType.isArrayType){
           if(fieldClass.thisType.toString != field.fieldType.toString){
-            println("YUP")
+            //println("YUP")
 
             count += 1
           }
-          println(s"DIFFERENT OBJECT : ${field.fieldType}")
+          //println(s"DIFFERENT OBJECT : ${field.fieldType}")
         }
       }
     }
     return count
   }
 }
-
-/*if(field.fieldType.isReferenceType){
-  if(!field.fieldType.toString.contains("java/") && !field.fieldType.isBaseType && !field.fieldType.isArrayType){
-    println(s"GOTCHA : ${field.fieldType}")
-  }
-}*/
