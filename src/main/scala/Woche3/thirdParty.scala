f9d3071476ce188f9e17178aca9b47c1e46725c1
MorisV <maurice.mecke@yahoo.de>
2025-06-03 13:47:35 +0200

    added powerMock dependency for analysis

--- src/main/scala/Woche3/thirdParty.scala
+++ src/main/scala/Woche3/thirdParty.scala
@@ -24,11 +24,15 @@ object thirdParty{
     val setOfusedMethodsSpring = mutable.Set.empty[(String, String)]
     val setOfpublicApiMethodsSpring = mutable.Set.empty[(String, String,String)]
     val setOfContainedMethodsSpring = mutable.Set.empty[(String,String,String)]
+    val setOfusedMethodsPowerMock = mutable.Set.empty[(String, String)]
+    val setOfpublicApiMethodsPowerMock = mutable.Set.empty[(String, String,String)]
+    val setOfContainedMethodsPowerMock = mutable.Set.empty[(String,String,String)]
 
     //LOADING JARS
     val jarFile = new java.io.File("openmrs-api-2.8.0-SNAPSHOT.jar")
     val hibernateFile = new java.io.File ("hibernate-core-5.6.15.Final.jar")
     val springFile = new java.io.File ("spring-core-5.3.30.jar")
+    val powerMockFile = new java.io.File ("powermock-reflect-2.0.9.jar")
 
     //GETTING THE INPUT
     var cgStrategy = ""
@@ -50,6 +54,7 @@ object thirdParty{
     val project = Project(jarFile, GlobalLogContext, config)
     val hibernateProject = Project (hibernateFile)
     val springProject = Project (springFile)
+    val powerMockProject = Project (powerMockFile)
 
     //GETTING HIBERNATE METHODS
     val classFilesHibernate = hibernateProject.allClassFiles
@@ -83,12 +88,29 @@ object thirdParty{
       }
     }
 
+    //GETTING POWERMOCK METHODS
+    val classFilesPowerMock = powerMockProject.allClassFiles
+    println(project.classFilesCount)
+    classFilesPowerMock.foreach{ classFile =>
+      val fqn = classFile.fqn
+      val splitFqn = fqn.split("/").mkString(".")
+      val methods = classFile.methods
+      methods.foreach{ method =>
+        println(method.fullyQualifiedSignature)
+        setOfContainedMethodsPowerMock += ((method.fullyQualifiedSignature,method.name, splitFqn))
+        if(method.isPublic && !method.isSynthetic && !classFile.thisType.packageName.contains("internal")){
+          setOfpublicApiMethodsPowerMock += ((method.fullyQualifiedSignature, method.name, splitFqn))
+        }
+      }
+    }
+
     //BUILDING CALLGRAPH OF THE WHOLE PROJECT
     val callGraph = callGraphStrategies(cgStrategy)
     val cg = project.get(callGraph)
     cg.reachableMethods.foreach(c => {
       cg.calleesOf(c.method).foreach(p => {
         p._2.foreach(u => {
+          //CHECKING IF HIBERNATE METHOD
           if(u.method.toString.contains("org.hibernate")) {
             setOfContainedMethodsHibernate.foreach((entry) => {
               if (entry._2 == u.method.name && u.method.toString().contains(entry._3)){
@@ -100,6 +122,7 @@ object thirdParty{
             })
             //println(s"--- ${u.toString} \n Name of the Method :  ${u.method.name} \n ")
           }
+          //CHECKING IF SPRING METHOD
           if(u.method.toString.contains("spring")) {
             setOfContainedMethodsSpring.foreach((entry) => {
               if (entry._2 == u.method.name && u.method.toString().contains(entry._3)){
@@ -111,19 +134,36 @@ object thirdParty{
             })
             //println(s"--- ${u.toString} \n Name of the Method :  ${u.method.name} \n ")
           }
+          //CHECKING IF POWERMOCK METHOD
+          if(u.method.toString.contains("org.powermock")) {
+            setOfContainedMethodsPowerMock.foreach((entry) => {
+              if (entry._2 == u.method.name && u.method.toString().contains(entry._3)){
+
+                //println(s"USED --- ${u.method} \n Name of the Method :  ${u.method.name} \n ")
+                //println(s"FQN : ${entry._1} NAME : ${entry._2} CLASS : ${entry._3} \n")
+                setOfusedMethodsPowerMock += ((u.method.toString, u.method.name))
+              }
+            })
+            //println(s"--- ${u.toString} \n Name of the Method :  ${u.method.name} \n ")
+          }
           })
         })
       })
 
-    /*setOfusedMethodsHibernate.foreach((entry) => {
+    /*setOfContainedMethodsPowerMock.foreach((entry) => {
       println(s"USED --- ${entry._1} \n Name of the Method :  ${entry._2} \n ")
     })*/
 
     println(s"Projects Method total : ${project.methodsCount}")
-    println(s"Used ${cgStrategy} callgraph \n --------------------------------------------------")
+    println(s"Used ${cgStrategy} callgraph \n --------------------------------------------------\n")
+    println("---------------HIBERNATE---------------")
     println(s"Total methods used from hibernate = ${setOfusedMethodsHibernate.size} of ${hibernateProject.methodsCount}")
     println(s"Total methods used public api hibernate = ${setOfusedMethodsHibernate.size} of ${setOfpublicApiMethodsHibernate.size} \n")
+    println("---------------SPRING---------------")
     println(s"Total methods used from Spring = ${setOfusedMethodsSpring.size} of ${springProject.methodsCount}")
-    println(s"Total methods used public api Spring = ${setOfusedMethodsSpring.size} of ${setOfpublicApiMethodsSpring.size}")
+    println(s"Total methods used public api Spring = ${setOfusedMethodsSpring.size} of ${setOfpublicApiMethodsSpring.size} \n")
+    println("---------------POWERMOCK---------------")
+    println(s"Total methods used from PowerMock = ${setOfusedMethodsPowerMock.size} of ${powerMockProject.methodsCount}")
+    println(s"Total methods used public api PowerMock = ${setOfusedMethodsPowerMock.size} of ${setOfpublicApiMethodsPowerMock.size}")
   }
 }
