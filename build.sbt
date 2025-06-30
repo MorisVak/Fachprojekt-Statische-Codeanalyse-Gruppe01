fd7c67534492b7f4af91a932975675f7d8d4ee4e
Dennis Pfeiffer <dennis.pfeiffer@tu-dortmund.de>
2025-06-30 15:54:45 +0200

    AbstractInterpretation rudimentary Implementation

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.13.12"
 
 libraryDependencies += "de.opal-project" % "framework_2.13" % "5.0.0"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.19" % "test"
+libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "3.0.0"
 
 ThisBuild / evictionErrorLevel := Level.Warn
 
--- src/main/scala/Woche5/AbstractInterpretation.scala
+++ src/main/scala/Woche5/AbstractInterpretation.scala
@@ -1,13 +1,17 @@
 package Woche5
 
+//import com.github.nscala_time.time.Imports.{DateTime, Duration, Interval, richDateTime, richReadableInstant}
 import org.opalj.ai.{AIResult, BaseAI}
 import org.opalj.ai.common.{DomainRegistry, XHTML}
 import org.opalj.br.analyses.Project
-
-import java.io.File
-import java.nio.file.{Files, Path}
-import scala.io.Source
 import play.api.libs.json._
+import org.joda.time.{DateTime, Duration}
+import org.opalj.br.Method
+
+import java.io.{File, PrintWriter}
+import java.nio.file.{Files, Path}
+import scala.collection.mutable
+import scala.io.Source
 
 object AbstractInterpretation {
 
@@ -24,9 +28,11 @@ object AbstractInterpretation {
     val domain = (json \ "domain").as[Int]
     println(domain)
 
-    val jar = (json \ "usedJar").as[String] // Hier ein Array von jars erstellen, um mehrere Dateien einzulesen
+    val jar = (json \ "usedJar").as[String] // Hier ein Array von jars ers
+    val ts = (json \ "test").as[List[String]]
 
-
+    println(jar)
+    println(ts)
 
     var cnt = 0
     val domainMap = domainDescriptions.map{ descr =>
@@ -43,51 +49,101 @@ object AbstractInterpretation {
     domainMap.foreach{ tuple =>
       println(s"\t [${tuple._1}] - ${tuple._2}")
     }
+    //set for keeping all the info needed for report
+    val foundMethods = mutable.Set.empty[(String, Int, Int, String, mutable.Set[(String, Int)])]
 
-    val projectJar = new File(jar)
+    val processStart : DateTime = DateTime.now()
 
-    val project = Project(projectJar)
+    ts.foreach { p =>
+      val projectJar = new File(p)
 
-    val ai = new BaseAI(true, false)
+      val project = Project(projectJar)
 
-    // Select a domain for the abstract interpretation
-    var domainIdentifierStr = domainMap(domain)
+      val ai = new BaseAI(true, false)
 
-    // This is needed because there is likely a bug in the way OPAL 5.0.0 handles domain identifiers.
-    domainIdentifierStr = domainIdentifierStr.substring(domainIdentifierStr.indexOf("]") + 2)
+      // Select a domain for the abstract interpretation
+      var domainIdentifierStr = domainMap(domain)
 
-    var methodCnt = 0
+      // This is needed because there is likely a bug in the way OPAL 5.0.0 handles domain identifiers.
+      domainIdentifierStr = domainIdentifierStr.substring(domainIdentifierStr.indexOf("]") + 2)
 
-    // Iterate over all class files in the project
-    project.allClassFiles.foreach{ classFile =>
+      var methodCnt = 0
 
-      // Iterate over all methods in the project
-      classFile.methods.foreach{ method =>
-        // We can only start abstract interpretation when there is code inside the method
-        if(method.body.isDefined){
-          // Build the domain object
-          val domain = DomainRegistry.newDomain(domainIdentifierStr, project, method)
+      // Iterate over all class files in the project
+      project.allClassFiles.foreach{ classFile =>
 
-          // Run the actual abstract interpretation for the current method using the interpreter and domain defined above
-          val result: AIResult = ai(method, domain)
+        // Iterate over all methods in the project
+        classFile.methods.foreach{ method =>
+          // We can only start abstract interpretation when there is code inside the method
+          if(method.body.isDefined){
+            // Build the domain object
+            val domain = DomainRegistry.newDomain(domainIdentifierStr, project, method)
 
-          val methodCode = result.code
-          val evaluatedInstructionPCs = result.evaluatedPCs
+            // Run the actual abstract interpretation for the current method using the interpreter and domain defined above
+            val result: AIResult = ai(method, domain)
 
-          // If the abstract interpretation evaluated (looked at) less instructions than there are in the code, that means
-          // there are instructions that are not reachable at all!
-          if(evaluatedInstructionPCs.size < methodCode.size){
-            println(s"${methodCode.size - evaluatedInstructionPCs.size} instructions not reachable in ${method.fullyQualifiedSignature}")
+            val methodCode = result.code
+            val evaluatedInstructionPCs = result.evaluatedPCs
+            val deadInstrCount = methodCode.size - evaluatedInstructionPCs.size
+
+            // If the abstract interpretation evaluated (looked at) less instructions than there are in the code, that means
+            // there are instructions that are not reachable at all!
+            if(evaluatedInstructionPCs.size < methodCode.size){
+              println(s"${methodCode.size - evaluatedInstructionPCs.size} instructions not reachable in ${method.fullyQualifiedSignature}")
+              val deadInstructions = mutable.Set.empty[(String, Int)]
+              methodCode.foreach { instr =>
+                if(!evaluatedInstructionPCs.contains(instr.pc)) deadInstructions += ((instr.instruction.toString, instr.pc))
+              }
+              foundMethods += ((method.fullyQualifiedSignature, methodCode.size, deadInstrCount, classFile.thisType.fqn, deadInstructions ))
+            }
+
+            methodCnt += 1
+
           }
-
-//          val rep = XHTML.dump(classFile, method, "Abstract Interpretation Succeeded", result).toString()
-//
-//          Files.writeString(Path.of("ai-data", s"$methodCnt-${method.name.replace("<", "").replace(">", "")}.html"), rep)
-          methodCnt += 1
-
         }
       }
     }
+
+    val processEnd:DateTime = DateTime.now()
+
+    val totalRuntime: Duration = new Duration(processStart, processEnd)
+    val totalRuntimeMs: Long = totalRuntime.getMillis
+
+    val pE = processEnd.toString()
+
+    val methodJsons: Seq[JsObject] = foundMethods.toSeq.map {
+      case (signature, totalInstr, deadInstr, className, deadInstrs) =>
+        val deadInstrJsons = deadInstrs.map {
+          case (instr, pc) => Json.obj(
+            "stringRepresentation" -> instr,
+            "programCounter" -> pc
+          )
+        }.toSeq
+
+        Json.obj(
+          "fullSignature" -> signature,
+          "numberOfTotalInstructions" -> totalInstr,
+          "numberOfDeadInstructions" -> deadInstr,
+          "enclosingTypeName" -> className,
+          "deadInstructions" -> deadInstrJsons
+        )
+    }
+
+
+    val reportJson: JsObject = Json.obj(
+      "filesAnalyzed" -> ts,
+      "domainUsed" -> s"$domain",
+      ("timeFinished" -> pE),
+      "totalRuntimeMs" -> totalRuntimeMs,
+      "methodsFound" -> methodJsons
+    )
+
+    val jsonString = Json.prettyPrint(reportJson)
+
+    val writer = new PrintWriter("report.json")
+    try writer.write(jsonString)
+    finally writer.close()
+
   }
 
 }
