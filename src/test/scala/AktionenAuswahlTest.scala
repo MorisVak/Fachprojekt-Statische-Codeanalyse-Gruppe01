e96fac9b716fea634513fe94d0df545e576fdc67
Dennis Pfeiffer <dennis.pfeiffer@tu-dortmund.de>
2025-07-29 22:15:50 +0200

    added Tests and fixed input json

--- src/test/scala/AktionenAuswahlTest.scala
+++ src/test/scala/AktionenAuswahlTest.scala
@@ -3,12 +3,6 @@ import org.scalatest.funsuite.AnyFunSuite
 import java.io.File
 
 class AktionenAuswahlTest extends AnyFunSuite {
-
-  test("Log created") {
-    Woche7.AktionenAuswahl
-    val file = new File("Logs.log")
-    assert(file.exists())
-  }
   test("Result directory and results created") {
     Woche7.AktionenAuswahl
     val f1 = new File(" Results/architechtureValidation_result.txt")
